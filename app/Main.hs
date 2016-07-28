070f1d073c6f0788c5e2f0ff89f95b4f07515bb0
Alexander Tchitchigin <sad.ronin@gmail.com>
2016-07-28 19:40:11 +0300

    Running dummy script using console driver in the main app.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
+import Commands
 import Lib
 
+import Driver.Console
+
+dummyScript :: Script ()
+dummyScript = do
+    forward 5.5
+    clockwise 30.0
+    forward 3.2
+
 main :: IO ()
-main = someFunc
+main = run dummyScript

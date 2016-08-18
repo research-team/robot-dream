3852d24f8aa7f73ced54df801764f3f7b9a46a7e
Adramalek <beherith10.02@gmail.com>
2016-08-19 00:02:33 +0300

    Change a little the body of dummyScript function.

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,16 @@ module Main where
 
 import Commands
 import Lib
+import System
 
 import Driver.Console
 
 dummyScript :: Script ()
 dummyScript = do
-    forward 5.5
-    clockwise 30.0
-    forward 3.2
+    forward chrs 5.5
+    clockwise chrs 30.0
+    forward chrs 3.2
+  where chrs = Vehical 0.2 (\x -> (*1.8) . fromIntegral $ x) (\x -> x*0.25)
 
 main :: IO ()
 main = run dummyScript

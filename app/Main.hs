96a3d9bb3bfe53d0231aae3eaebdbac9f3fb17b9
Adramalek <beherith10.02@gmail.com>
2016-08-22 19:33:32 +0300

    Correct an import block of code according to renaming of module 'System' to 'Configuration'.

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Commands
 import Lib
-import System
+import Configuration
 
 import Driver.Console
 
@@ -11,7 +11,7 @@ dummyScript = do
     forward chrs 5.5
     clockwise chrs 30.0
     forward chrs 3.2
-  where chrs = Vehical 0.2 (\x -> (*1.8) . fromIntegral $ x) (\x -> x*0.25)
+  where chrs = Vehical 0.2 0.5 (\x -> (*1.8) . fromIntegral $ x) (\x -> x*0.25)
 
 main :: IO ()
 main = run dummyScript

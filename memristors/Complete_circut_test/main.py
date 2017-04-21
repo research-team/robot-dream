6da16cd0511a575e32b2358a0be33f21188cf8a8
YurBurGer <yurger2009@gmail.com>
2017-04-21 14:39:26 +0300

    Added lots of test data and scripts

--- memristors/Complete_circut_test/main.py
+++ memristors/Complete_circut_test/main.py
@@ -1,18 +1,26 @@
 import matplotlib.pyplot as plt
 
-f = open('Data2.txt', 'r')
+f = open('Main.TNO', 'r')
+#f = open('Data.txt', 'r')
 data=[]
 noshiftdata=[]
 p1=[]
 p2=[]
-T0=0.0048
-T1=0.0049
+T0=0.0049
+T1=0.005
 lp1=0;
 lp2=0;
 s=f.readline()
-while s!="":
+d=[]
+res=[]
+while '(Secs)' not in s:
+    s = f.readline()
+s = f.readline()
+print s
+while (s!="")and(' ' in s):
     #print s
     d=map(float,s.split())
+    #rint d
     data.append(d)
     #print data
     if d[1]>3.6:
@@ -28,7 +36,6 @@ while s!="":
 #print p1
 #rising g_pul
 #print p2
-res=[]
 for d in p1:
     if d in p2:
         res.append(d)
@@ -49,19 +56,25 @@ while p1[ind1][0]-p2[ind2][0]<T1:
     i2=data.index(p2[ind2+1])
     #print [i1,i2]
     maxs=data[i1]
-    maxh=data[i1]
+    minh=data[i1]
     #print data[i1:i2]
     for d in data[i1:i2]:
         if d[3]>maxs[3]:
             maxs=d
-        if d[4]>maxh[4]:
-            maxh=d
-    x.append(cur1[0]-cur2[0])
+        if d[4]>minh[4]:
+            minh=d
+    x.append(cur2[0]-cur1[0])
     sombrero.append(maxs[3])
-    hebb.append(maxh[4])
+    hebb.append(minh[4])
     ind1-=1
     ind2-=1
 #print sombrero
+x.reverse()
+sombrero.reverse()
+hebb.reverse()
+#print x
+ind1=p1.index(res[1])#g_inp pulses
+ind2=p2.index(res[1])#g_pul pulses
 while p2[ind2][0]-p1[ind1][0]<T1:
     cur1=p1[ind1]
     cur2=p2[ind2]
@@ -72,21 +85,21 @@ while p2[ind2][0]-p1[ind1][0]<T1:
     i2=data.index(cur3)
     #print [i1,i2]
     maxs=data[i1]
-    maxh=data[i1]
+    minh=data[i1]
     #print data[i1:i2]
     for d in data[i1:i2]:
         if d[3]>maxs[3]:
             maxs=d
-        if d[4]>maxh[4]:
-            maxh=d
-    x.append(cur1[0]-cur2[0])
+        if d[4]>minh[4]:
+            minh=d
+    x.append(cur2[0]-cur1[0])
     sombrero.append(maxs[3])
-    hebb.append(maxh[4])
+    hebb.append(minh[4])
     ind1+=1
     ind2+=1
-smb=plt.plot(x,sombrero)
-plt.setp(smb, color='r', linewidth=2.0)
-plt.show()
+#print x
+#smb=plt.plot(x,sombrero)
+#plt.setp(smb, color='r', linewidth=2.0)
 heb=plt.plot(x,hebb)
 plt.setp(heb, color='g', linewidth=2.0)
 plt.show()

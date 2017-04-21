c37e90a9ff20cfed8f5656a4762678dc180dee68
Yuriy Gerasimov <yurger2009@gmail.com>
2017-04-21 19:38:14 +0300

    Testing new schema+ updated results

--- memristors/Complete_circut_test/test.py
+++ memristors/Complete_circut_test/test.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 
-#f = open('Main.TNO', 'r')
-f = open('Data2.txt', 'r')
+f = open('Main.TNO', 'r')
+#f = open('Data2.txt', 'r')
 data=[]
 p1=[]
 p2=[]
-T0=0.0048
-T1=0.0049
-Pdt=0.01
+T0=0.0050
+T1=0.0051
+Pdt=0
 lp1=0;
 lp2=0;
 s=f.readline()
 d=[]
 res=[]
-#while '(Secs)' not in s:
-#    s = f.readline()
-#s = f.readline()
+while '(Secs)' not in s:
+    s = f.readline()
+s = f.readline()
 print s
 
 while (s!="")and(' ' in s):
@@ -24,7 +24,7 @@ while (s!="")and(' ' in s):
     #rint d
     data.append(d)
     #print data
-    if d[1]>3.6:
+    if d[1]==5:
         if (d[0]-lp1)>T0:
             p1.append(d)
             lp1=d[0]
@@ -53,24 +53,26 @@ for p2p in p2:
     ind1=data.index(p2p)
     ind2 = data.index(p2p1)
     maxs=p2p[3]
-    maxh=p2p[4]
-    minh = p2p[4]
+    maxh = p2p[4]
+    minh=p2p[4]
     for d in data[ind1:ind2]:
         if d[3]>maxs:
             maxs=d[3]
-        if (dt%T0)<=(T0/2):
-            if maxh>d[4]:
-                maxh=d[4]
-        else:
-            if maxh<d[4]:
-                maxh=d[4]
+        if d[4]> maxh:
+            maxh=d[4]
+        if d[4]<minh:
+            minh=d[4]
     sombrero.append(maxs)
-    hebb.append(maxh)
-x.remove(x[0])
-sombrero.remove(sombrero[0])
-hebb.remove(hebb[0])
-#smb=plt.plot(x,sombrero)
-#plt.setp(smb, color='r', linewidth=2.0)
+    if (dt%T0)<(T0/2):
+        hebb.append(maxh)
+    else:
+        hebb.append(minh)
+#smb=plt.plot(x[24:75],sombrero[24:75])
+smb=plt.plot(x,sombrero)
+plt.setp(smb, color='r', linewidth=2.0)
+#plt.show()
+#heb=plt.plot(x[24:75],hebb[24:75])
+#xx=map(lambda x: x - T1/2, x)
 heb=plt.plot(x,hebb)
 plt.setp(heb, color='g', linewidth=2.0)
 plt.show()
